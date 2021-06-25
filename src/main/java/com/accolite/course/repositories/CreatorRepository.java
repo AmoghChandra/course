766ec848afcf2c50180090833fd71e174ec966fb
AmoghChandra <amogharchandra1999@gmail.com>
2021-06-25 12:00:30 +0530

    Second commit

---
+++ src/main/java/com/accolite/course/repositories/CreatorRepository.java
@@ -0,0 +1,18 @@
+package com.accolite.course.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.accolite.course.entities.CourseEntity;
+
+
+	@Repository
+	public interface CreatorRepository extends CrudRepository<CourseEntity, Long> {
+
+		Optional<CourseEntity> findByLocation(String location);
+
+		
+	}
+
---
+++ src/main/java/com/accolite/course/repositories/SkillRepository.java
@@ -0,0 +1,16 @@
+package com.accolite.course.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.accolite.course.entities.CourseEntity;
+
+@Repository
+public interface SkillRepository extends CrudRepository<CourseEntity, Long> {
+
+	Optional<CourseEntity> findByLocation(String location);
+
+	
+}
